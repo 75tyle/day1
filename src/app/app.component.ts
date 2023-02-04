f852670fc69594b86e97fa838320330888e1a904
sanghpal <kamblesanghpal2003@gmail.com>
2023-02-04 19:55:25 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'day1';
+export class AppComponent implements OnInit {
+  colorred ={
+    color:"red"
+  }
+text:string = ""
+  ngOnInit(): void {
+    $(document).ready(function(){
+      console.log("ksldfsfj")
+
+     
+    })
+  }
+  
+  onsubmit(){
+    console.log("gfhgfg")
+    this.text = $('.user_text').val();
+    this.pop()
+  }
+  pop(){
+    console.log('popup called')
+  this.colorred.color = "blue"
+    $('.container_msgbox').css('display','flex')
+    setTimeout(function() {
+        $('.container_msgbox').css('display','none')
+    }, 3000);
+  }
 }
